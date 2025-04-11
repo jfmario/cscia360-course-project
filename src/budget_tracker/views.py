8f952fc7a9757c91014d53dae1ebac41d69285ef
John Marion <johnmarion@Johns-MacBook-Pro-2.local>
2025-04-11 13:55:30 -0400

    reorganize structure and add blank home page

--- src/budget_tracker/views.py
+++ src/budget_tracker/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_page(request):
-    return 
+    return render(request, 'index.html')
--- src/course_project/urls.py
+++ src/course_project/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import budget_tracker.views
+
 urlpatterns = [
+    path('', budget_tracker.views.home_page),
     path('admin/', admin.site.urls),
 ]
